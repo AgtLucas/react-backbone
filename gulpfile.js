c140cd00196b174a47ba19c07776395508e53911
Lucas <lucas@agtlucas.com>
2014-12-26 08:40:15 -0200

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,16 @@
 var gulp = require('gulp')
   , browserify = require('browserify')
   ;
+
+// Browserify task
+gulp.task('browserify', function () {
+  var browserified = transform(function (filename) {
+    var b = browserify(filename);
+    return b.bundle();
+  });
+
+  return gulp.src(['./assets/src/js/*.js'])
+    .pipe(browserified)
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets/build/js'));
+});
